3fa77ed63bec1929262466b945188cc23a9e7121
YaCpotato <yasshisshy@gmail.com>
2019-09-16 17:46:30 +0900

    細かいバグ修正

--- app/static.py
+++ app/static.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-app = Blueprint('public', __name__, static_url_path='', static_folder='../public')
+app = Blueprint('public', __name__, static_url_path='', static_folder='../public/templates/')
